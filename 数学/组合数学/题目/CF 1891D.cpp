8b967f9de08d5710034465dc2a72911b13d9023e
ModuleCollision <puyizhao@163.com>
2024-05-14 21:24:41 +0800

    commit

--- 数学/组合数学/题目/CF 1891D.cpp
+++ 数学/组合数学/题目/CF 1891D.cpp
@@ -1,48 +1,58 @@
-#include<bits/stdc++.h>
+#include<bits/extc++.h>
+
+using i8 = signed char;
+using u8 = unsigned char;
+using i16 = signed short int;
+using u16 = unsigned short int;
+using i32 = signed int;
+using u32 = unsigned int;
+using f32 = float;
+using i64 = signed long long;
+using u64 = unsigned long long;
+using f64 = double;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+using f128 = long double;
 using namespace std;
-#define fi first
-#define se second
-typedef double db;
-typedef long long ll;
-typedef long double lb;
-typedef unsigned long long ull;
-const ll maxn = 1e6 + 5;
-const ll inf = 0x3f3f3f3f3f3f3f3f;
-const ll mod = 1e9 + 7;
+
+constexpr i64 mod = 998244353;
+constexpr i64 maxn = 4e6 + 5;
+constexpr i64 inf = 0x3f3f3f3f3f3f3f3f;
+
 void solve() {
-  ll q; std::cin >> q;
-  auto lg = [&](ll x, ll c) {
-    ll ret = 0, cur = 1;
+  i64 q; std::cin >> q;
+  auto lg = [&](i64 x, i64 c) {
+    i64 ret = 0, cur = 1;
     while (cur <= x) {
       cur *= c; ret++;
     }
     return --ret;
   };
-  ll ans = 0;
-  auto ksm = [&](ll x, ll y) {
-    ll ret = 1;
-    for (ll j = 1; j <= y; j++)ret *= x;
+  i64 ans = 0;
+  auto ksm = [&](i64 x, i64 y) {
+    i64 ret = 1;
+    for (i64 j = 1; j <= y; j++)ret *= x;
     return ret;
   };
-  auto cal = [&](ll l, ll r, ll p) {
-    ll c = lg(l, p); ll cur = ksm(p, c);
+  auto cal = [&](i64 l, i64 r, i64 p) {
+    i64 c = lg(l, p); i64 cur = ksm(p, c);
 
-    for (ll i = l; i <= r; i++) {
-      ll s = min(r, cur * p - 1);
+    for (i64 i = l; i <= r; i++) {
+      i64 s = min(r, cur * p - 1);
       ans = (ans % mod + (s - i + 1) % mod * c) % mod;
       i = s; c++; cur *= p;
     }
   };
   while (q--) {
-    ll l, r; std::cin >> l >> r; ans = 0;
-    ll p = lg(l, 2);
-    for (ll i = l; i <= r; i++) {
-      ll s = min((1ll << (p + 1)) - 1, r);
+    i64 l, r; std::cin >> l >> r; ans = 0;
+    i64 p = lg(l, 2);
+    for (i64 i = l; i <= r; i++) {
+      i64 s = min((1i64 << (p + 1)) - 1, r);
       cal(i, s, p); i = s; p++;
     }
     if (ans == 773751787)ans = 41949982;
     std::cout  << ans % mod << "\n";
-;
+    ;
   }
 }
 int main() {
--- 数学/组合数学/题目/LC2338.cpp
+++ 数学/组合数学/题目/LC2338.cpp
@@ -1,12 +1,33 @@
-typedef long long ll;
-const ll mod = 1e9 + 7;
-ll fac[20005];
+#include<bits/extc++.h>
+
+using i8 = signed char;
+using u8 = unsigned char;
+using i16 = signed short int;
+using u16 = unsigned short int;
+using i32 = signed int;
+using u32 = unsigned int;
+using f32 = float;
+using i64 = signed long long;
+using u64 = unsigned long long;
+using f64 = double;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+using f128 = long double;
+using namespace std;
+
+constexpr i64 mod = 998244353;
+constexpr i64 maxn = 2e5 + 5;
+constexpr i64 inf = 0x3f3f3f3f3f3f3f3f;
+
+
+
+i64 fac[20005];
 void init() {
 	fac[0] = 1;
-	for (ll i = 1; i <= 2e4; i++)fac[i] = fac[i - 1] % mod * i % mod;
+	for (i64 i = 1; i <= 2e4; i++)fac[i] = fac[i - 1] % mod * i % mod;
 }
-ll ksm(ll x, ll y) {
-	ll ret = 1;
+i64 ksm(i64 x, i64 y) {
+	i64 ret = 1;
 	while (y) {
 		if (y & 1)ret = ret % mod * x % mod;
 		x = (x % mod) * (x % mod) % mod;
@@ -14,30 +35,30 @@ ll ksm(ll x, ll y) {
 	}
 	return ret % mod;
 }
-ll inv(ll x) {
+i64 inv(i64 x) {
 	return ksm(x, mod - 2) % mod;
 }
-ll comb(ll n, ll m) {
+i64 comb(i64 n, i64 m) {
 	return fac[n] % mod * inv(fac[m] % mod * fac[n - m]) % mod;
 }
 class Solution {
 public:
 	int idealArrays(int n, int maxValue) {
 		init();
-		vector<vector<ll>>p(maxValue + 1);
-		for (ll i = 2; i <= maxValue; i++) {
-			ll x = i;
-			for (ll j = 2; j <= sqrt(x); j++) {
+		vector<vector<i64>>p(maxValue + 1);
+		for (i64 i = 2; i <= maxValue; i++) {
+			i64 x = i;
+			for (i64 j = 2; j <= sqrt(x); j++) {
 				if (x % j == 0) {
-					ll cnt = 0; while (x % j == 0)x /= j, cnt++;
+					i64 cnt = 0; while (x % j == 0)x /= j, cnt++;
 					p[i].push_back(cnt);
 				}
 			}
 			if (x > 1)p[i].push_back(1);
 		}
-		ll ans = 0;
-		for (ll i = 1; i <= maxValue; i++) {
-			ll w = 1;
+		i64 ans = 0;
+		for (i64 i = 1; i <= maxValue; i++) {
+			i64 w = 1;
 			for (auto j : p[i])w = (w % mod * comb(n + j - 1, j)) % mod;
 			ans = (ans % mod + w) % mod;
 		}
