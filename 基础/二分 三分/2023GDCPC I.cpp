8250ee0462b9926d7ceed4529dcbddae19359520
ModuleCollision <puyizhao@163.com>
2024-05-15 16:04:13 +0800

    commit

--- 基础/二分 三分/2023GDCPC I.cpp
+++ 基础/二分 三分/2023GDCPC I.cpp
@@ -1,11 +1,25 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include<bits/extc++.h>
+
+using i8 = signed char;
+using u8 = unsigned char;
+using i16 = signed short int;
+using u16 = unsigned short int;
+using i32 = signed int;
+using u32 = unsigned int;
+using f32 = float;
+using i64 = signed long long;
+using u64 = unsigned long long;
 using f64 = double;
-using i64 = long long;
-typedef long double lb;
-const i64 maxn = 5e3 + 5;
-const i64 inf = 0x3f3f3f3f3f3f3f3f;
-const i64 mod = 1e9 + 7;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+using f128 = long double;
+using namespace std;
+
+constexpr i64 mod = 998244353;
+constexpr i64 maxn = 4e6 + 5;
+constexpr i64 inf = 0x3f3f3f3f3f3f3f3f;
+
+
 void solve() {
 	i64 n, m; std::cin >> n >> m;
 	vector<pair<i64, i64>>p(n * m + 5);
--- 数学/多项式 生成函数/题目/NC2023多校7F.cpp
+++ 数学/多项式 生成函数/题目/NC2023多校7F.cpp
@@ -1,11 +1,26 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include<bits/extc++.h>
+
+using i8 = signed char;
+using u8 = unsigned char;
+using i16 = signed short int;
+using u16 = unsigned short int;
+using i32 = signed int;
+using u32 = unsigned int;
+using f32 = float;
+using i64 = signed long long;
+using u64 = unsigned long long;
 using f64 = double;
-using i64 = long long;
-typedef long double lb;
-const i64 maxn = 1e6 + 5;
-const i64 inf = 0x3f3f3f3f3f3f3f3f;
-const i64 mod = 998244353;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+using f128 = long double;
+using namespace std;
+
+constexpr i64 mod = 998244353;
+constexpr i64 maxn = 4e6 + 5;
+constexpr i64 inf = 0x3f3f3f3f3f3f3f3f;
+
+
+
 i64 norm(i64 x) {
 	return (x % mod + mod) % mod;
 }
