0da5ee9b14eb9ecebe3a85186ac5d77c366ea669
ModuleCollision <puyizhao@163.com>
2024-05-18 23:37:35 +0800

    commit

--- 基础/二分 三分/LC1235.cpp
+++ 基础/二分 三分/LC1235.cpp
@@ -27,3 +27,68 @@ public:
         return dp[n];
     }
 };
+
+
+
+
+
+#include<bits/stdc++.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+using i8 = signed char;
+using u8 = unsigned char;
+using i16 = signed short int;
+using u16 = unsigned short int;
+using i32 = signed int;
+using u32 = unsigned int;
+using f32 = float;
+using i64 = signed long long;
+using u64 = unsigned long long;
+using f64 = double;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+using f128 = long double;
+
+using namespace std;
+const i64 mod = 1e9 + 7;
+const i64 maxn = 1e6 + 5;
+const i64 inf = 0x3f3f3f3f3f3f3f3f;
+
+
+
+
+/* 不满足单调栈上二分的性质, 不能单调栈上二分 */
+class Solution {
+public:
+    i64 tr[maxn];
+
+    void add(i64 idx, i64 v) {
+        for (; idx <= 100000; idx += (idx & -idx))tr[idx] = std::max(tr[idx], v);
+    }
+
+    i64 query(i64 idx) {
+        i64 ret = 0;
+        for (; idx >= 1; idx -= (idx & -idx))ret = std::max(ret, tr[idx]);
+        return ret;
+    }
+    int jobScheduling(vector<int> &startTime, vector<int> &endTime, vector<int> &profit) {
+        i64 n = startTime.size();
+        std::vector<array<i64, 3>>z(1);
+        for (i64 i = 0; i < n; i++) {
+            z.push_back({startTime[i], endTime[i], profit[i]});
+        }
+        std::sort(z.begin() + 1, z.begin() + 1 + n, [&](auto x, auto y)->bool{
+            return x[1] < y[1];
+        });
+        vector<i64>dp(n + 5);
+        for (i64 i = 1; i <= n; i++) {
+            i64 start = z[i][0];
+            i64 idx = lower_bound(z.begin(), z.begin() + i, start, [&](array<i64, 3>x, i64 y) -> bool{
+                return x[1] <= y;
+            }) - z.begin() - 1;
+            dp[i] = std::max(dp[i], query(idx) + z[i][2]);
+            add(i, dp[i]);
+        }
+        return query(n);
+    }
+};
