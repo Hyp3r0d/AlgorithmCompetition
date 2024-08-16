7b515c0f213ad3cfa6450a89728f368d8c2ce051
ModuleCollision <puyizhao@163.com>
2024-08-16 18:25:24 +0800

    参赛作品提交

--- 数学/线性代数/题目/LGP3389.cpp
+++ 数学/线性代数/题目/LGP3389.cpp
@@ -27,13 +27,13 @@ std::vector<i64>w(105, -1);
 void Gauss(vector<vector<f64>>& matrix, i64 n, i64 m) {
 	i64 lead = 0;// 该行主元所在列
 	for (i64 r = 0; r < n; ++r) {
-		if (lead == n) return;
+		if (lead >= m - 1) return; // 主元只能在系数矩阵中
 		i64 i = r;
 		while (fabs(matrix[i][lead]) < eps) {
 			++i;
 			if (i == n) {
 				i = r; ++lead;
-				if (lead == n) return;
+				if (lead == m - 1) return;
 			}
 		}
 		w[r] = lead;
@@ -62,24 +62,22 @@ void Gauss(vector<vector<f64>>& matrix, i64 n, i64 m) {
 
 
 
+
 int main() {
 	i64 n ; std::cin >> n;
-	std::vector a(n + 5, std::vector<f64>(n + 5, 0.0));
+	std::vector a(100 + 5, std::vector<f64>(100 + 5, 0.0));
 	for (i64 i = 0; i < n; i++) {
 		for (i64 j = 0; j <= n; j++)std::cin >> a[i][j];
 	}
 	Gauss(a, n, n + 1);
-	/*for (i64 i = 0; i < n; i++) {
-		for (i64 j = 0; j <= n; j++) {
-			std::cout << fixed << setprecision(4) << a[i][j] << " \n"[j == n];
-		}
-	}*/		
+
+
 	i64 x = 0, y = n - 1;
 	while (x < n and w[x] != -1)x++;
 	x--;
 	while (y >= 0 and abs(a[y][n]) < eps)y--;
-	
-	if (x < y) {
+
+	if (x < y or x < n - 1) {
 		puts("No Solution");
 		return 0;
 	}
