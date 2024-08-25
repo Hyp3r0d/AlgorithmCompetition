a47fec876d3d47e0a7916fddd73b2398b7583265
ModuleCollision <puyizhao@163.com>
2024-08-26 07:49:27 +0800

    参赛作品提交

--- 动态规划/图上dp/LGP2151.cpp
+++ 动态规划/图上dp/LGP2151.cpp
@@ -1,5 +1,4 @@
-#include<bits/extc++.h>
-
+#include<bits/stdc++.h>
 using i8 = signed char;
 using u8 = unsigned char;
 using i16 = signed short int;
@@ -14,33 +13,26 @@ using i128 = __int128_t;
 using u128 = __uint128_t;
 using f128 = long double;
 using namespace std;
+const i64 mod = 45989;
+const i64 maxn = 1e6 + 5;
+const i64 inf = 0x3f3f3f3f3f3f3f3f;
 
-constexpr i64 mod = 45989;
-constexpr i64 maxn = 4e6 + 5;
-constexpr i64 inf = 0x3f3f3f3f3f3f3f3f;
 
 struct Matrix {
 public:
-	i64 n; std::vector<std::vector<i64>>a;
-	Matrix(i64 len = 4): n(len), a(len) {
-		for (i64 i = 0; i < len; i++) {
-			a[i].assign(len, 0);
-		}
-	}
-	void init() {
+	i64 n, m;
+	vector<vector<i64>>a;
+	Matrix(i64 n, i64 m): n(n), m(m) {
+		a.resize(n);
 		for (i64 i = 0; i < n; i++) {
-			for (i64 j = 0; j < n; j++) {
-				if (i == j)a[i][j] = 1;
-				else a[i][j] = 0;
-			}
+			a[i].resize(m, 0);
 		}
 	}
 	friend Matrix operator*(Matrix x, Matrix y) {
-		i64 n = x.n;
-		Matrix ret(n);
-		for (i64 i = 0; i < n; i++) {
-			for (i64 j = 0; j < n; j++) {
-				for (i64 k = 0; k < n; k++) {
+		Matrix ret(x.n, y.m);
+		for (i64 i = 0; i < x.n; i++) {
+			for (i64 j = 0; j < y.m; j++) {
+				for (i64 k = 0; k < x.m; k++) {
 					ret.a[i][j] = (ret.a[i][j] % mod + x.a[i][k] % mod * y.a[k][j] % mod) % mod;
 				}
 			}
@@ -48,16 +40,16 @@ public:
 		return ret;
 	}
 	friend Matrix operator+(Matrix x, Matrix y) {
-		Matrix ret(x.n);
+		Matrix ret(x.n, x.m);
 		for (i64 i = 0; i < x.n; i++) {
-			for (i64 j = 0;	 j < y.n; j++) {
-				ret.a[i][j] = (x.a[i][j] % mod + y.a[i][j]) % mod;
+			for (i64 j = 0; j < x.m; j++) {
+				ret.a[i][j] = (ret.a[i][j] % mod + x.a[i][j] % mod + y.a[i][j] % mod) % mod;
 			}
 		}
 		return ret;
 	}
 	friend Matrix quickPower(Matrix x, i64 y) {
-		Matrix ret(x.n);
+		Matrix ret(x.n, x.n);
 		for (i64 i = 0; i < x.n; i++)ret.a[i][i] = 1;
 		while (y) {
 			if (y & 1)ret = ret * x;
@@ -65,27 +57,46 @@ public:
 			y >>= 1;
 		}
 		return ret;
-	};
+	}
 };
 
 
+struct edge {
+	i64 v, nxt;
+} e[maxn]; i64 cnt = 0;
+i64 head[maxn];
+void addEdge(i64 u, i64 v) {
+	e[cnt].v = v; e[cnt].nxt = head[u]; head[u] = cnt++;
+}
+
 int main() {
+	memset(head, -1, sizeof(head));
 	i64 n, m, t, a, b; std::cin >> n >> m >> t >> a >> b;
-	std::vector<vector<i64>>g(n + 1);
 	for (i64 i = 1; i <= m; i++) {
 		i64 u, v; std::cin >> u >> v;
-		g[u].push_back(v);
-		g[v].push_back(u);
+		addEdge(u, v); addEdge(v, u);
 	}
-	Matrix c(n);
-	c.a[a][0] = 1;
-	Matrix trans(n);
-	for (i64 i = 0; i < n; i++) {
-		for (auto v : g[i]) {
-			trans.a[i][v] = 1;
+	Matrix g(cnt, cnt);
+	for (i64 z = head[a]; z != -1; z = e[z].nxt) {
+		g.a[0][z] = 1;
+	}
+	Matrix trans(cnt, cnt);
+	for (i64 i = 0; i < cnt; i++) {
+		i64 v = e[i].v;
+		for (i64 j = head[v]; j != -1; j = e[j].nxt) {
+			if ((j ^ 1) != i) {
+				trans.a[i][j] += 1;
+				trans.a[i][j] %= mod;
+			}
 		}
 	}
-	c = quickPower(trans, t) * c;
-	i64 ans = c.a[b][0] % mod;
+
+	g = g * quickPower(trans, t - 1);
+
+	i64 ans = 0;
+	for (i64 i = head[b]; i != -1; i = e[i].nxt) {
+		ans = (ans % mod + g.a[0][i ^ 1]) % mod;
+	}
 	std::cout << ans % mod << "\n";
+	return 0;
 }
