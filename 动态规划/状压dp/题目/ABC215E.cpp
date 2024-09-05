98f84a9281ac0355aeac7a0483c3be9bbcbaef0b
ModuleCollision <puyizhao@163.com>
2024-09-05 20:12:56 +0800

    update

--- 动态规划/状压dp/题目/ABC215E.cpp
+++ 动态规划/状压dp/题目/ABC215E.cpp
@@ -49,3 +49,65 @@ void solve() {
 int main() {
 	solve();
 }
+
+
+
+/*多组Test下使用封装数据结构, 在函数中开对象,避免暴力清空数组造成时间浪费*/
+
+/*STL: 数据结构 std::cin std::cout  function类 lowerbound builtin max min accumulate iota stoi atoi等函数需要加上std,避免命名空间污染*/
+
+/*数学先打表, 后对拍, 图论只会暴力就bitset搞*/
+
+/*300兆字节 1- 2 秒*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+using i8 = signed char;
+using u8 = unsigned char;
+using i16 = signed short int;
+using u16 = unsigned short int;
+using i32 = signed int;
+using u32 = unsigned int;
+using f32 = float;
+using i64 = signed long long;
+using u64 = unsigned long long;
+using f64 = double;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+using f128 = __float128;
+constexpr i64 mod = 998244353;
+constexpr i64 maxn = 2e6 + 5;
+constexpr i64 inf = 0x3f3f3f3f3f3f3f3f;
+
+int main() {
+	i64 n; std::cin >> n;
+	string s; std::cin >> s; s = " " + s;
+	std::vector dp(n + 1, std::vector((1 << 12), std::vector<i64>(11, 0)));
+
+	dp[0][0][10] = 1;
+
+	for (i64 i = 1; i <= n; i++) {
+		for (i64 j = 0; j <= (1 << 10) - 1; j++) {
+			for (i64 k = 0; k <= 10; k++) {
+				dp[i][j][k] = (dp[i][j][k] % mod + dp[i - 1][j][k]) % mod;
+			}
+		}
+
+		i64 pre = s[i] - 'A';
+		for (i64 j = 0; j <= (1 << 10) - 1; j++) {
+			if ((j >> pre) & 1) {
+				dp[i][j][pre] = (dp[i][j][pre] % mod + dp[i - 1][j][pre]) % mod;
+				for (i64 k = 0; k <= 10; k++) {
+					dp[i][j][pre] = (dp[i][j][pre] % mod + dp[i - 1][j - (1 << pre)][k]) % mod;
+				}
+			}
+		}
+	}
+	i64 ans = 0;
+	for (i64 i = 0; i <= (1 << 10) - 1; i++) {
+		for (i64 j = 0; j <= 9; j++)ans = (ans % mod + dp[n][i][j]) % mod;
+	}
+	std::cout << ans % mod << "\n";
+
+}
