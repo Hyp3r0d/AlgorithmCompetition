c86fa2ff4715221f6487ee7534b13ad0e32af9e1
ModuleCollision <puyizhao@163.com>
2025-03-05 21:50:51 +0800

    update

--- 字符串/括号匹配 表达式求值/LC10.cpp
+++ 字符串/括号匹配 表达式求值/LC10.cpp
@@ -23,23 +23,24 @@ class Solution {
 public:
 	bool isMatch(string s, string p) {
 		int n = s.size(), m = p.size();
-		bool dp[n + 5][m + 5];
-		memset(dp, false, sizeof(dp));
+		std::vector dp(n + 1, std::vector<bool>(m + 1, false));
 		s = " " + s; p = " " + p;
 		dp[0][0] = true;
 		for (int i = 1; i <= m; i++) {
-			if (p[i] == '*' and i >= 2)dp[0][i] |= dp[0][i - 2];
+			if (p[i] == '*' and i >= 2)dp[0][i] = dp[0][i] | dp[0][i - 2];
 		}
 		for (i64 i = 1; i <= n; i++) {
 			for (i64 j = 1; j <= m; j++) {
-				if ((isalpha(p[j]) and p[j] == s[i]) or p[j] == '.')dp[i][j] |= dp[i - 1][j - 1];
-				else {
+				if (p[j] == s[i] or p[j] == '.') {
+					dp[i][j] = dp[i][j] | dp[i - 1][j - 1];
+				}
+				else if (p[j] == '*') {
 					if (p[j - 1] == '.') {
-						for (i64 k = i; k >= 0; k--)dp[i][j] |= dp[k][j - 2];
+						for (i64 k = i; k >= 0; k--)dp[i][j] = dp[i][j] | dp[k][j - 2];
 					} else {
 						i64 k = i; while (s[k] == p[j - 1] and k >= 1)k--;
-						if (j > 1)
-							for (i64 w = i; w >= k; w--)dp[i][j] |= dp[w][j - 2];
+						if (j >= 2)
+							for (i64 w = i; w >= k; w--)dp[i][j] = dp[i][j] | dp[w][j - 2];
 					}
 				}
 			}
