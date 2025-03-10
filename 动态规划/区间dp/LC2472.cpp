1c9925a2e853365e821dc1d2b1a6b4bff7d979ac
ModuleCollision <puyizhao@163.com>
2025-03-10 23:11:51 +0800

    update

--- 动态规划/区间dp/LC2472.cpp
+++ 动态规划/区间dp/LC2472.cpp
@@ -1,30 +1,45 @@
+#include<bits/stdc++.h>
+using i8 = signed char;
+using u8 = unsigned char;
+using i16 = signed short int;
+using u16 = unsigned short int;
+using i32 = signed int;
+using u32 = unsigned int;
+using f32 = float;
+using i64 = signed long long;
+using u64 = unsigned long long;
+using f64 = double;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+using f128 = long double;
+using namespace std;
+constexpr i64 mod = 998244353;
+constexpr i64 maxn = 2e5 + 5;
+constexpr i64 inf = 0x3f3f3f3f3f3f3f3f;
+
 class Solution {
 public:
     int maxPalindromes(string s, int k) {
-        int n = s.size();
-        std::vector is(n + 5, std::vector<bool>(n + 5, false));
+        i64 n = s.size();
         s = " " + s;
-        for (int i = 0; i <= n; i++) {
-            is[i][i] = 1;
-            is[i + 1][i] = 1;
-        }
-        for (int len = 2; len <= n; len++) {
-            for (int l = 1; l + len - 1 <= n; l++) {
-                int r = l + len - 1;
-                if (s[l] == s[r]) {
-                    is[l][r] = is[l][r] | is[l + 1][r - 1];
-                }
+        std::vector isPalindrome(n + 1, std::vector<bool>(n + 1, false));
+        for (i64 i = 1; i <= n; i++) {
+            isPalindrome[i][i] = true;
+            if (i <= n - 1 and s[i] == s[i + 1]) {
+                isPalindrome[i][i + 1] = true;
             }
         }
-        std::vector<int>dp(n + 1, -0x3f3f3f3f);
-        dp[0] = 0;
-        for (int i = 1; i <= n; i++) {
-            dp[i] = max(dp[i], dp[i - 1]);
-            if (i >= k) {
-                for (int j = i - k; j >= 0; j--) {
-                    if (is[j + 1][i]) {
-                        dp[i] = max(dp[i], dp[j] + 1);
-                    }
+        for (i64 len = 3; len <= n; len++) {
+            for (i64 i = 1; i + len - 1 <= n; i++) {
+                isPalindrome[i][i + len - 1] = isPalindrome[i + 1][i + len - 2] && (s[i] == s[i + len - 1]);
+            }
+        }
+        std::vector<i64>dp(n + 1);
+        for (i64 i = 1; i <= n; i++) {
+            dp[i] = std::max(dp[i], dp[i - 1]);
+            for (i64 j = i - k; j >= 0; j--) {
+                if (isPalindrome[j + 1][i] == true) {
+                    dp[i] = std::max(dp[i], dp[j] + 1);
                 }
             }
         }
